6f1619feb929f5aca52c2df4d79c02f0cfebfec3
jjong <jjong@google.com>
2020-03-05 00:53:26 -0800

    Add InfraValidator into an Iris Keras native example.
    PiperOrigin-RevId: 299040858

--- tfx/examples/iris/iris_pipeline_native_keras.py
+++ tfx/examples/iris/iris_pipeline_native_keras.py
@@ -27,6 +27,7 @@ import tensorflow_model_analysis as tfma
 from tfx.components import CsvExampleGen
 from tfx.components import Evaluator
 from tfx.components import ExampleValidator
+from tfx.components import InfraValidator
 from tfx.components import Pusher
 from tfx.components import ResolverNode
 from tfx.components import SchemaGen
@@ -39,6 +40,7 @@ from tfx.dsl.experimental import latest_blessed_model_resolver
 from tfx.orchestration import metadata
 from tfx.orchestration import pipeline
 from tfx.orchestration.beam.beam_dag_runner import BeamDagRunner
+from tfx.proto import infra_validator_pb2
 from tfx.proto import pusher_pb2
 from tfx.proto import trainer_pb2
 from tfx.types import Channel
@@ -104,8 +106,8 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
       examples=transform.outputs['transformed_examples'],
       transform_graph=transform.outputs['transform_graph'],
       schema=schema_gen.outputs['schema'],
-      train_args=trainer_pb2.TrainArgs(num_steps=10000),
-      eval_args=trainer_pb2.EvalArgs(num_steps=5000))
+      train_args=trainer_pb2.TrainArgs(num_steps=2000),
+      eval_args=trainer_pb2.EvalArgs(num_steps=5))
 
   # Get the latest blessed model for model validation.
   model_resolver = ResolverNode(
@@ -138,11 +140,26 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
       # Change threshold will be ignored if there is no baseline (first run).
       eval_config=eval_config)
 
+  # Performs infra validation of a candidate model to prevent unservable model
+  # from being pushed. This config will launch a model server of the latest
+  # TensorFlow Serving image in a local docker engine.
+  infra_validator = InfraValidator(
+      model=trainer.outputs['model'],
+      examples=example_gen.outputs['examples'],
+      serving_spec=infra_validator_pb2.ServingSpec(
+          tensorflow_serving=infra_validator_pb2.TensorFlowServing(
+              tags=['latest']),
+          local_docker=infra_validator_pb2.LocalDockerConfig()),
+      request_spec=infra_validator_pb2.RequestSpec(
+          tensorflow_serving=infra_validator_pb2.TensorFlowServingRequestSpec())
+  )
+
   # Checks whether the model passed the validation steps and pushes the model
   # to a file destination if check passed.
   pusher = Pusher(
       model=trainer.outputs['model'],
       model_blessing=evaluator.outputs['blessing'],
+      infra_blessing=infra_validator.outputs['blessing'],
       push_destination=pusher_pb2.PushDestination(
           filesystem=pusher_pb2.PushDestination.Filesystem(
               base_directory=serving_model_dir)))
@@ -159,6 +176,7 @@ def _create_pipeline(pipeline_name: Text, pipeline_root: Text, data_root: Text,
           trainer,
           model_resolver,
           evaluator,
+          infra_validator,
           pusher,
       ],
       enable_cache=True,
